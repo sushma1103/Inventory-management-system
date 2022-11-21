5fdef0210d477eb79fe6f246b3a5aa5035ff3089
anitalh <anitalhegde@gmail.com>
2022-11-20 17:09:46 -0800

    Adding user and customer classes

--- InventoryManagementSystem/src/pojo/Customer.java
+++ InventoryManagementSystem/src/pojo/Customer.java
@@ -1,5 +1,71 @@
 package pojo;
 
 public class Customer {
-
+	
+	private String customerId;
+	private String customerCode;
+	private String name;
+	private String location;
+	private String phone;
+	private String email;
+	
+	// Getters for customerId
+	public String getCustomerId() {
+		return customerId;
+	}
+	
+	// Setters for customerId
+	public void setCustomerId(String customerId) {
+		this.customerId = customerId;
+	}
+	
+	// Getters for customerCode
+	public String getCustomerCode() {
+		return customerCode;
+	}
+	
+	//Setters for customerCode
+	public void setCustomerCode(String customerCode) {
+		this.customerCode = customerCode;
+	}
+	
+	// Getters for name
+	public String getName() {
+		return name;
+	}
+	
+	// Setters for name
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	// Getters for location
+	public String getLocation() {
+		return location;
+	}
+	
+	// Setters for location
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+	// Getters for phone
+	public String getPhone() {
+		return phone;
+	}
+	
+	// Setters for phone
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	// Getters for email
+	public String getEmail() {
+		return email;
+	}
+	
+	// Setters for email
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
--- InventoryManagementSystem/src/pojo/User.java
+++ InventoryManagementSystem/src/pojo/User.java
@@ -1,5 +1,71 @@
 package pojo;
 
 public class User {
-
+	
+	private String userId;
+	private String name;
+    private String location;
+    private String phone;
+    private String category;
+    private String email;
+    
+    // Getters for userId
+	public String getUserId() {
+		return userId;
+	}
+	
+	// Setters for userId
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+	// Getters for name
+	public String getName() {
+		return name;
+	}
+	
+	// Setters for name
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	// Getters for location
+	public String getLocation() {
+		return location;
+	}
+	
+	// Setters for location
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+	// Getters for phone
+	public String getPhone() {
+		return phone;
+	}
+	
+	// Setters for phone
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	// Getters for category
+	public String getCategory() {
+		return category;
+	}
+	
+	// Setters for category
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	// Getters for email
+	public String getEmail() {
+		return email;
+	}
+	
+	// Setters for email
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
